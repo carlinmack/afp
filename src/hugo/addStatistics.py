a32178d764ebcd618ed830529ff8db6a42a5b9c4
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2020-09-30 00:25:36 +0100

    Add rest of statistics

---
+++ src/hugo/addStatistics.py
@@ -0,0 +1,49 @@
+# need to add `return data` at line 212 in templates.py
+
+import os
+import sys
+import json
+
+sys.path.append('admin/sitegen-lib')
+
+import templates
+from config import options, release_pattern
+from sitegen import parse, associate_releases, read_versions
+import metadata
+import afpstats
+
+def addStatistics():
+    dataDir = "data/"
+    options.templates_dir = "metadata/templates"
+
+    entries = parse(os.path.join("metadata", "metadata"))
+    versions = read_versions(os.path.join("metadata", "release-dates"))
+    associate_releases(entries, versions, os.path.join("metadata", "releases"))
+
+    deps_dict = metadata.empty_deps(entries)
+
+    afp_dict = afpstats.afp_dict(entries, "thys", deps_dict)
+    afp_dict.build_stats()
+    builder = templates.Builder(options, entries, afp_dict)
+
+    stats = builder.generate_statistics()
+
+    loc_articles = [ article.loc for article in stats["articles_by_time"] ];
+
+    all_articles = [ a.name for a in stats["articles_by_time"] ]
+
+    data = {"num_lemmas": stats["num_lemmas"],
+              "num_loc": stats["num_loc"],
+              "articles_year": stats["articles_year"],
+              "loc_years": stats["loc_years"],
+              "author_years": stats["author_years"],
+              "author_years_cumulative": stats["author_years_cumulative"], 
+              "loc_articles": loc_articles,
+              "all_articles": all_articles}
+    
+    with open(dataDir + "statistics.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    addStatistics()
--- src/hugo/exportMetadata.py
+++ src/hugo/exportMetadata.py
@@ -3,6 +3,7 @@ import time
 from addOlderReleases import addOlderReleases
 from iniToJson import iniToJson
 from addDependencies import addDependencies
+from addStatistics import addStatistics
 
 if __name__ == "__main__":
     tick = time.time()
@@ -10,6 +11,8 @@ if __name__ == "__main__":
     iniToJson()
     addOlderReleases()
     addDependencies()
+    # for this to work add `return data` at line 212 in templates.py
+    addStatistics()
 
     print("--- %s seconds ---" % (time.time() - tick))
 
