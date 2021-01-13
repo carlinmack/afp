74fd5a1869895bfc05e4cdac2ba0482797857e11
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2021-01-13 16:05:51 +0000

    Add theories to entry pages

--- src/afp-devel/admin/exportMetadata.py
+++ src/afp-devel/admin/exportMetadata.py
@@ -5,13 +5,14 @@ from addOlderReleases import addOlderReleases
 from addStatistics import addStatistics
 from iniToJson import iniToJson
 from relatedEntries import addRelatedEntries
+from addTheories import addTheories
 
 def updateProgressBar(desc, t):
     t.update()
     t.set_description(desc)
 
 if __name__ == "__main__":
-    t = tqdm(total=5, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} {elapsed_s:.0f}s')
+    t = tqdm(total=6, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} {elapsed_s:.0f}s')
     
     updateProgressBar("Export metadata to JSON", t)
     iniToJson()
@@ -21,6 +22,9 @@ if __name__ == "__main__":
 
     updateProgressBar("Add dependencies", t)
     addDependencies()
+    
+    updateProgressBar("Add theories", t)
+    addTheories()
 
     updateProgressBar("Export statistics", t)
     # for this to work add `return data` at line 212 in templates.py
