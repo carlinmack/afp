eafcef570553ff87920aa31fb33495acfa572b42
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2022-02-21 17:20:43 +0000

    Update users schema

--- src/afp-devel/admin/hugo/api/boot/db.js
+++ src/afp-devel/admin/hugo/api/boot/db.js
@@ -4,12 +4,11 @@ var db = require('../db');
 module.exports = function() {
 
   db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS users ( \
-      email TEXT UNIQUE, \
-      hashed_password BLOB, \
-      salt BLOB, \
-      name TEXT \
-    )");
+    db.run(
+        'CREATE TABLE users (username TEXT UNIQUE, email TEXT UNIQUE, \
+    hashed_password BLOB, salt BLOB, name TEXT, image TEXT, affiliation TEXT, \
+    website TEXT, description TEXT);)'
+    );
   });
 
   //db.close();
