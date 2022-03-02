38c3fe62bb4a2c8877df54e66a21d09fa654d021
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2022-03-02 12:23:43 +0000

    Remove debug code

--- src/afp-devel/admin/hugo/api/comments.js
+++ src/afp-devel/admin/hugo/api/comments.js
@@ -4,13 +4,7 @@ var db = require('./db.js');
 var router = express.Router();
 
 // URL prefix: /api/comment/
-router.get('/', function (req, res) {
-    console.log('wolrd');
-  res.send('Hello World');
-});
-
 router.get('/mostRecent', function (req, res, next) {
-    console.log("hello")
     db.get(
         'select max(id) as mostRecent from comments',
         function (err, row) {
